25f270e62518ea8b3392289a5cf3bba5f579f507
vedika82 <anandvedika420@gmail.com>
2026-02-12 23:24:32 +0530

    bafaltu ke commits

--- router/friends.js
+++ router/friends.js
@@ -11,16 +11,16 @@ let friends = {
 
 // GET request: Retrieve all friends
 router.get("/",(req,res)=>{
+    // Send JSON response with formatted friends data
+    res.send(JSON.stringify(friends,null,4));
 
-  // Update the code here
-
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
-router.get("/:email",(req,res)=>{
-  // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+router.get('/:email', function(req, res) {
+    // Retrieve the email parameter from the request URL and send the corresponding friend's details
+    const email = req.params.email;
+    res.send(friends[email]);
 });
 
 
